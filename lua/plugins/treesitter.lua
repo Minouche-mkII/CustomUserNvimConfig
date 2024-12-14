2f2202225f95ee3e683c65613a0b0cc0faf23a5b
elouan <elouan.rival@etu.univ-lyon1.fr>
2024-12-14 12:08:56 +0100

    neo tree et compilateur zig

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
     config = function()
+        require ("nvim-treesitter.install").compilers = { "zig" };
         require("nvim-treesitter.configs").setup({
             ensure_installed = {"lua", "c_sharp", "java", "html", "css", "javascript"},
             highlight = { enabled = true },
